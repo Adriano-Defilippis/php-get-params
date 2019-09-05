c775ada84b44001900d26bbe13149c931d8aa1e7
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-09-05 19:13:44 +0200

    Aggiunto index.php e readme.txt

---
+++ README.txt
@@ -0,0 +1,10 @@
+TO VIEW THE PAGE:
+-----------------------------
+
+1| Install a Web server in local host (MAMP);
+
+2| Connect the web server at the folder project;
+
+3| Run from Browser Web the URL whit a parameters for get function of PHP: localhost/index.php?len=length&wid=width&hei=heigth;
+
+4| len, wid, hei, are parameters to be entered manually in the url; 
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- FONT: LATO -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <!-- FONT: FONTAWESOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+    <!-- JS: JQUERY -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- JS: MOMENT -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
+    <!-- TEMPLATE: MESSAGE MENU -->
+    <script id="item-template" type="text/x-handlebars-template">
+    </script>
+
+    <!-- CSS: MY STYLE -->
+    <link rel="stylesheet" href="style.css">
+    <!-- JS: MY SCRIPT -->
+    <script src="script.js" charset="utf-8"></script>
+    <!-- IMG: ICON -->
+    <link rel="shortcut icon" href="img/me_icon.gif">
+    <title>MyFirst PHP Project</title>
+
+    <?php 
+        $hello = "Hello world!";
+
+        $myArr = ["ciap", "mamma", "addio"];
+    ?>
+    
+  </head>
+  <body>
+  <h1> 
+  <?php
+  echo($hello ."<br>");
+  echo($myArr ."<br>");
+
+  var_dump($hello);
+  echo("<br>");
+  var_dump($myArr);
+  ?>
+   </h1>
+
+  
+  </body>
+</html>
